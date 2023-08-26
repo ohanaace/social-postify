bd3e7a8b0c774d3c184cd47c2ae27e72398cc1de
suelen <sreginaclsilva@gmail.com>
2023-08-26 13:59:47 -0300

    build: create application modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { MediaModule } from './media/media.module';
+import { PostsModule } from './posts/posts.module';
+import { PublicationsModule } from './publications/publications.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, MediaModule, PostsModule, PublicationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
